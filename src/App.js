ecf47fee34a94659a16610b0d9a4b40adb252660
fjccpm <fjccpm@gmail.com>
2022-06-24 21:33:19 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
 import React from "react";
 import Dice from "./Dice";
+import Confetti from "react-confetti";
+//import useWindowSize from 'react-use/lib/useWindowSize'
 
 function App() {
 
   const [dices, setDices] = React.useState([])
 
+  //selected is set with the value of the first dice blocked. 
   const [selected, setSelected] = React.useState(-1)
+
+  //tenzies will change tu true when you win game.
+  const [tenzies, setTenzies] = React.useState(false)
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max)+1;
   }
 
+/*React.useEffect(
+  () => {
+    if(selected!==-1)
+    {
+      let temp=true
+      for(let i=0; i<10; i++)
+      {
+        if(dices[i].value!==selected) {
+          temp=false
+        }  
+      }
+      setTenzies(temp)
+      
+      if(tenzies) {
+       alert("you win!!!")
+      }
+    }
+    // eslint-disable-next-line
+  }, [dices]
+)*/
+
+React.useEffect(
+  () => {
+      if(selected !== -1)
+      {
+        const allBlocked = dices.every(die => die.on===true)
+        const allEqual = dices.every(die => die.value===selected)
+        if(allBlocked && allEqual)
+        {
+          setTenzies(true)
+          //alert("You Win!!!")
+        }
+      }
+    // eslint-disable-next-line
+  }, [dices]
+)
+
  React.useEffect(
   () => {
-    console.log("Initializing Dices!")
-    if(dices.length===0)
+    initializeGame()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []
+ )
+
+ function initializeGame() {
+  console.log("Initializing Dices!")
+  if(dices.length===0 || tenzies)
     {
       let temp=[]
       for(let i=0; i<10; i++)
@@ -28,10 +77,9 @@ function App() {
             }]
       }
       setDices(temp)
+      setTenzies(false)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []
- )
+ }
 
  function block(id) {
   let temp
@@ -59,7 +107,12 @@ function App() {
 }
 
 function rollDice() {
-  let temp=[]
+  if(tenzies)
+  {
+    initializeGame()
+  }
+  else {
+    let temp=[]
       for(let i=0; i<10; i++)
       { 
       //console.log(i)
@@ -71,6 +124,7 @@ function rollDice() {
             }]
       }
       setDices(temp)
+    }
 }
 
  const dicesElements = dices.map(dice => (
@@ -85,6 +139,7 @@ function rollDice() {
  
   return (
     <main className="tenzies--main">
+      {tenzies && <Confetti />}
       <div className="tenzies--container">
         <div className="tenzies--tittle">Tenzies</div>
         <div className="tenzies--subtittle">Roll until all dice are the same. 
@@ -92,7 +147,7 @@ function rollDice() {
         <div className="tenzies--dices--container">{dicesElements}</div>
         <div className="tenzies--roll--button"
           onClick={rollDice}>
-          <p className="tenzies--roll--button--text">Roll</p>
+          <p className="tenzies--roll--button--text">{tenzies ? "Restart" : "Roll"}</p>
         </div>
       </div>
     </main>
